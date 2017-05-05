e81f0b64cb1ee39002007aa177a416d44475d0ec
Ana Ausek <anac_ausek@hotmail.com>
2017-05-05 19:58:22 -0400

    final deploy

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+//Setting up MySQL Connection
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	port: 3306,
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "burgers_db"
+});
+
+//Make connection
+connection.connect(function (err) {
+	if (err) {
+		console.error("Error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+//Export connection for ORM use
+module.exports = connection;
