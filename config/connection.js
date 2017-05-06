ccc07f6f534887f1fd152f7a0dd2eac72b133641
Ana Ausek <anac_ausek@hotmail.com>
2017-05-05 20:52:25 -0400

    connection.js update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 //Setting up MySQL Connection
-var mysql = require("mysql"),
-	connection;
+var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-	} else {
-		connection = mysql.createConnection({
+		connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
 		port: 3306,
 		host: "localhost",
 		user: "root",
@@ -15,13 +15,7 @@ if (process.env.JAWSDB_URL) {
 };
 
 //Make connection
-connection.connect(function (err) {
-	if (err) {
-		console.error("Error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 //Export connection for ORM use
 module.exports = connection;
